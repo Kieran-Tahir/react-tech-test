06b8f7bda000b1676245eec8c40bf6b75a22626f
Kieran <kierantahir@gmail.com>
2023-10-11 12:24:45 +1300

    complete slides challenge

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Slides({slides}) {
+function Slides({ slides }) {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button data-testid="button-restart" className="small outlined" onClick={() => setCurrentSlide(0)}>
+          Restart
+        </button>
+        <button
+          data-testid="button-prev"
+          className="small"
+          disabled={currentSlide === 0 ? true : false}
+          onClick={() => setCurrentSlide(currentSlide - 1)}
+        >
+          Prev
+        </button>
 
+        <button
+          data-testid="button-next"
+          className="small"
+          disabled={currentSlide === (slides.length - 1) ? true : false}
+          onClick={() => setCurrentSlide(currentSlide + 1)}
+        >
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{slides[currentSlide].title}</h1>
+        <p data-testid="text">{slides[currentSlide].text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Slides;
